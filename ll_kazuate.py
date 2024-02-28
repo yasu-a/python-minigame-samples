a9d9b366555f24a6f814198fd7d5bfe229c5a08f
prograb-metro-tp49 <yasuhiro.gtgt@gmail.com>
2024-02-28 18:07:04 +0900

    added kazuate

---
+++ ll_kazuate.py
@@ -0,0 +1,22 @@
+import random
+
+r = random.randint(1, 100)
+count = 0
+while True:
+    try:
+        i = int(input(f"1から100までの数字を当ててください（{count + 1}回目） ?> "))
+    except ValueError:
+        print("数字を入力してね")
+        continue
+    else:
+        if not (1 <= i <= 100):
+            print("1から100までの数字だよ")
+            continue
+    count += 1
+    if i > r:
+        print("正解はもっと小さい！")
+    elif i < r:
+        print("正解はもっと大きい！")
+    else:
+        break
+print(f"正解！{count}回で成功！")
